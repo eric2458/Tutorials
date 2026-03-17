6f4fc2e5e820b4fd95b27fb3dd0a9bb409268032
eric2458 <59672148+eric2458@users.noreply.github.com>
2026-03-16 22:58:07 -0700

    Player input and movement
    Player now has movement and input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,34 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 { 
-    [SerializeField]public float moveSpeed = 7f;
+    private bool isWalking;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
   private void Update()
   {
-      Vector2 inputVector = new Vector2(0, 0);
-        if (Keyboard.current.wKey.isPressed)
-        {
-            inputVector.y = +1;
-            //Debug.Log("Pressed W");
-        }
-        if (Keyboard.current.sKey.isPressed)
-        {
-            inputVector.y = -1;
-            //Debug.Log("Pressed A");
-        }if (Keyboard.current.aKey.isPressed)
-        {
-            inputVector.x = -1;
-            //Debug.Log("Pressed S");
-        }if (Keyboard.current.dKey.isPressed)
-        {
-            inputVector.x = +1;
-            //Debug.Log("Pressed D");
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime; 
-        Debug.Log(Time.deltaTime);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        //Debug.Log(Time.deltaTime);
+  }
+
+  public bool IsWalking()
+  {
+      return isWalking;
   }
 }
