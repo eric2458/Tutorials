1df667eb83cb0311012042c798d768528c264370
eric2458 <59672148+eric2458@users.noreply.github.com>
2026-04-05 20:16:37 -0700

    Music added
    Music has been added

---
+++ Assets/Scripts/ScriptableObjects/RecipeSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+[CreateAssetMenu()]
+public class RecipeSO : ScriptableObject
+{
+    public List<KitchenObjectSO> kitchenObjectSOList;
+    public string recipeName;
+}
