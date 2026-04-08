3239f5aa40131a36097778e232447c6a6d7302fe
eric2458 <59672148+eric2458@users.noreply.github.com>
2026-04-08 09:19:34 -0700

    Pause menu
    Added Pause menu with options

--- Assets/Scripts/Counters/CuttingCounter.cs
+++ Assets/Scripts/Counters/CuttingCounter.cs
@@ -4,6 +4,11 @@ using System;
 public class CuttingCounter : BaseCounter, IHasProgress
 {
 	public static event EventHandler OnAnyCut;
+
+	new public static void ResetStaticData()
+	{
+		OnAnyCut = null;
+	}
 	public event EventHandler<IHasProgress.OnProgressChangedEventArgs> OnProgressChanged;
 	public event EventHandler OnCut;
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
